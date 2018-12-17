73103656d88bac34b488f49f8143b49a1cfd8468
Kamani, Kelvin <kelvin.kamani@emtecinc.com>
2018-12-17 17:31:06 +0530

    setup first passign react test

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,7 +1,13 @@
+import "jest-dom/extend-expect";
+import React from "react";
+import { render } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import App from "./app";
+
 describe("app", () =>
 {
     it("Runs and passes", () =>
     {
-        expect(true).toBe(true);
+        render(<App />)
     })
 })
