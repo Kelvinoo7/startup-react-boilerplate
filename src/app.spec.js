11d34fedc22ef61f69ba4478b953b191a97b3724
Kamani, Kelvin <kelvin.kamani@emtecinc.com>
2018-12-17 17:34:49 +0530

    setup globals and common imports

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,8 +1,7 @@
-import "jest-dom/extend-expect";
 import React from "react";
 import { render } from "react-testing-library";
-import "react-testing-library/cleanup-after-each";
 import App from "./app";
+
 
 describe("app", () =>
 {
