9479baf06a46f8e914dd2ac370c5d20e4f0d4b2b
Kelvin.Kamani <kamani_kelvin@yahoo.com>
2020-08-21 13:50:32 +0530

    updated packages and configured test cases

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,10 +1,10 @@
-var merge = require("webpack-merge");
+var { merge } = require("webpack-merge");
 var baseConfig = require("./webpack.config.base");
 
 module.exports = merge(baseConfig, {
   mode: "development",
   devServer: {
-    port: 9000
+    port: 9000,
   },
-  devtool: "source-map"
+  devtool: "source-map",
 });
